650bd3b00b5424b4ff077c0d9cbdf006f006ff31
brytsknguyen <thienminh.npn@gmail.com>
2021-04-28 17:02:01 +0800

    revised the scripts

--- launch/run_one_bag_ntuviral.sh
+++ launch/run_one_bag_ntuviral.sh
@@ -2,7 +2,7 @@
 
 export EPOC_DIR=$1;
 export DATASET_LOCATION=$2;
-export VIRAL_DIR=$3;
+export ROS_PKG_DIR=$3;
 export EXP_NAME=$4;
 export CAPTURE_SCREEN=$5;
 export LOG_DATA=$6;
@@ -19,31 +19,25 @@ echo "BAG DURATION:" $BAG_DUR "=> LOG_DUR:" $LOG_DUR;
 
 let ANC_MAX=ANC_ID_MAX+1
 
-export VIRAL2_OUTPUT_DIR=$EPOC_DIR/result_${EXP_NAME}_${ANC_MAX}anc;
+export ALOAM_OUTPUT_DIR=$EPOC_DIR/result_${EXP_NAME}_${ANC_MAX}anc;
 if ((FUSE_VIS==1))
 then
-export VIRAL2_OUTPUT_DIR=${VIRAL2_OUTPUT_DIR}_vis;
+export ALOAM_OUTPUT_DIR=${ALOAM_OUTPUT_DIR}_vis;
 fi
-echo OUTPUT DIR: $VIRAL2_OUTPUT_DIR;
+echo OUTPUT DIR: $ALOAM_OUTPUT_DIR;
 
 export BA_LOOP_LOG_DIR=/home/$USER;
 if $LOG_DATA
 then
-export BA_LOOP_LOG_DIR=$VIRAL2_OUTPUT_DIR;
+export BA_LOOP_LOG_DIR=$ALOAM_OUTPUT_DIR;
 fi
 echo BA LOG DIR: $BA_LOOP_LOG_DIR;
 
-mkdir -p $VIRAL2_OUTPUT_DIR/ ;
-cp -R $VIRAL_DIR/config $VIRAL2_OUTPUT_DIR;
-cp -R $VIRAL_DIR/launch $VIRAL2_OUTPUT_DIR;
-roslaunch viral2 run_ntuviral.launch log_dir:=$VIRAL2_OUTPUT_DIR \
-fuse_uwb:=$FUSE_UWB \
-fuse_vis:=$FUSE_VIS \
-uwb_dis_bias:=$UWB_BIAS \
-anc_idx_max:=$ANC_ID_MAX \
+mkdir -p $ALOAM_OUTPUT_DIR/ ;
+cp -R $ROS_PKG_DIR/config $ALOAM_OUTPUT_DIR;
+cp -R $ROS_PKG_DIR/launch $ALOAM_OUTPUT_DIR;
+roslaunch aloam run_ntuviral.launch log_dir:=$ALOAM_OUTPUT_DIR \
 autorun:=true \
-ba_log_filename:=$BA_LOOP_LOG_DIR/viral_ba_log.csv \
-loop_log_filename:=$BA_LOOP_LOG_DIR/viral_loop_log.csv \
 bag_file:=$DATASET_LOCATION/$EXP_NAME/$EXP_NAME.bag \
 & \
 
@@ -52,7 +46,7 @@ then
 echo CAPTURING SCREEN ON;
 sleep 1;
 ffmpeg -video_size 1920x1080 -framerate 1 -f x11grab -i :0.0+1920,0 \
--loglevel quiet -t $LOG_DUR -y $VIRAL2_OUTPUT_DIR/$EXP_NAME.mp4 \
+-loglevel quiet -t $LOG_DUR -y $ALOAM_OUTPUT_DIR/$EXP_NAME.mp4 \
 & \
 else
 echo CAPTURING SCREEN OFF;
@@ -63,24 +57,24 @@ if $LOG_DATA
 then
 echo LOGGING ON;
 sleep 5;
-rosparam dump $VIRAL2_OUTPUT_DIR/allparams.yaml;
+rosparam dump $ALOAM_OUTPUT_DIR/allparams.yaml;
 timeout $LOG_DUR rostopic echo -p --nostr --noarr /viral2_odometry/pred_odom \
-> $VIRAL2_OUTPUT_DIR/predict_odom.csv \
+> $ALOAM_OUTPUT_DIR/predict_odom.csv \
 & \
 timeout $LOG_DUR rostopic echo -p --nostr --noarr /viral2_odometry/opt_odom \
-> $VIRAL2_OUTPUT_DIR/opt_odom.csv \
+> $ALOAM_OUTPUT_DIR/opt_odom.csv \
 & \
 timeout $LOG_DUR rostopic echo -p --nostr --noarr /leica/pose/relative \
-> $VIRAL2_OUTPUT_DIR/leica_pose.csv \
+> $ALOAM_OUTPUT_DIR/leica_pose.csv \
 & \
 timeout $LOG_DUR rostopic echo -p --nostr --noarr /dji_sdk/imu \
-> $VIRAL2_OUTPUT_DIR/dji_sdk_imu.csv \
+> $ALOAM_OUTPUT_DIR/dji_sdk_imu.csv \
 & \
 timeout $LOG_DUR rostopic echo -p --nostr --noarr /imu/imu \
-> $VIRAL2_OUTPUT_DIR/vn100_imu.csv \
+> $ALOAM_OUTPUT_DIR/vn100_imu.csv \
 & \
 timeout $LOG_DUR rostopic echo -p --nostr --noarr /viral2_odometry/optimization_status \
-> $VIRAL2_OUTPUT_DIR/optimization_status.csv \
+> $ALOAM_OUTPUT_DIR/optimization_status.csv \
 ;
 else
 echo LOGGING OFF;
--- launch/runall_ntuviral.sh
+++ launch/runall_ntuviral.sh
@@ -12,14 +12,14 @@ cd $CURR_DIR
 echo CURRENT DIR: $CURR_DIR
 echo VIRAL DIR:   $PACKAGE_DIR
 
-export EPOC_DIR=/home/$USER/nucssd2/MATLAB_WS/RAL_edits/viralslam/RAL_VIRAL_SLAM/NTUVIRAL_20210428
+export EPOC_DIR=/home/$USER/nucssd2/MATLAB_WS/RAL_edits/viralslam/RAL_VIRAL_SLAM/ntuviral_aloam
 export DATASET_LOCATION=/home/$USER/DATASETS/NTU_VIRAL/
 # export DATASET_LOCATION=/media/$USER/myHPSSD/NTU_VIRAL
 
 export CAPTURE_SCREEN=false;
 export LOG_DATA=true;
 
-#region USE VIS, NO UWB -----------------------------------------------------------------------------------------------
+#region NO VIS, NO UWB ------------------------------------------------------------------------------------------------
 
 wait;
 ./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_01 $CAPTURE_SCREEN $LOG_DATA 450 0 1 0.75 -1;
@@ -42,137 +42,7 @@ wait;
 wait;
 ./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_03 $CAPTURE_SCREEN $LOG_DATA 450 0 1 0.75 -1;
 
-#endregion USE VIS, NO UWB --------------------------------------------------------------------------------------------
-
-
-#region USE VIS, USE UWB, 2 ANCHORS------------------------------------------------------------------------------------
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_01 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_02 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_03 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
- 
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_01 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_02 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_03 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_01 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_02 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_03 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 1;
-
-#endregion USE VIS, USE UWB, 2 ANCHORS --------------------------------------------------------------------------------
-
-
-#region USE VIS, USE UWB, 3 ANCHORS------------------------------------------------------------------------------------
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_01 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_02 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_03 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
- 
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_01 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_02 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_03 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_01 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_02 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_03 $CAPTURE_SCREEN $LOG_DATA 450 1 1 0.75 2;
-
-#endregion USE VIS, USE UWB, 3 ANCHORS --------------------------------------------------------------------------------
-
-
-#region NO VIS, NO UWB ------------------------------------------------------------------------------------------------
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_01 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_02 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_03 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
- 
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_01 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_02 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_03 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_01 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_02 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_03 $CAPTURE_SCREEN $LOG_DATA 450 0 0 0.75 -1;
-
 #endregion NO VIS, NO UWB ---------------------------------------------------------------------------------------------
-
-
-#region NO VIS, USE UWB, 2 ANCHORS---------------------------------------------------------------------------------------
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_01 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_02 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_03 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
- 
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_01 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_02 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_03 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_01 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_02 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_03 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 1;
-
-#endregion NO VIS, USE UWB, 2 ANCHORS ---------------------------------------------------------------------------------
-
-
-#region NO VIS, USE UWB, 3 ANCHORS---------------------------------------------------------------------------------------
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_01 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_02 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR eee_03 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
- 
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_01 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_02 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR nya_03 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_01 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_02 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-wait;
-./run_one_bag_ntuviral.sh $EPOC_DIR $DATASET_LOCATION $PACKAGE_DIR sbs_03 $CAPTURE_SCREEN $LOG_DATA 450 1 0 0.75 2;
-
-#endregion NO VIS, USE UWB, 3 ANCHORS ---------------------------------------------------------------------------------
 
 
 #region ## Poweroff ---------------------------------------------------------------------------------------------------
